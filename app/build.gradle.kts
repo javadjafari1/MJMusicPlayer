79acebe827ba6822c3c125344e6fcf4dc195bc71
Javad Jafari <javad2147@yahoo.com>
2023-09-28 12:41:48 +0330

    #6 add on_pull_request.yml github action that run build and detekt on every pull request on the develop branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,9 +66,6 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.0-alpha04")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$composeUiVersion")
 }
