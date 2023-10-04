8dad91f229308aa42c99ca5b350607350f653fe8
Mohammad-Taghizadeh <mohammad93p@yahoo.com>
2023-10-04 23:11:16 +0330

    #9 removing the inject viewModel in the mainActivity for not confirming the build

--- app/src/main/kotlin/ir/thatsmejavad/mjmusic/MainActivity.kt
+++ app/src/main/kotlin/ir/thatsmejavad/mjmusic/MainActivity.kt
@@ -3,10 +3,8 @@ package ir.thatsmejavad.mjmusic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import org.koin.android.ext.android.inject
 
 internal class MainActivity : ComponentActivity() {
-    private val viewModel: MainActivityViewModel by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {}
