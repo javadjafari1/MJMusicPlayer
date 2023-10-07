5f5e75af53e2d08a66d1d2025e1f5f1a92fd181b
Javad Jafari <javad2147@yahoo.com>
2023-10-07 23:15:30 +0330

    #15 add ksp and room dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
