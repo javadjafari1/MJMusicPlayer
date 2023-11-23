c2de827da94ddfcea22e32db9d4ee6bed6a46a03
Mohammad Yazdi <mohammadyt1379@gmail.com>
2023-11-23 22:03:59 +0330

    #27 - Add Media3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,10 @@ dependencies {
 
     //accompanist
     implementation("com.google.accompanist:accompanist-navigation-material:0.32.0")
+
+    //Media3
+    implementation("androidx.media3:media3-exoplayer:1.1.1")
+    implementation("androidx.media3:media3-session:1.1.1")
 }
 
 class RoomSchemaArgProvider(
