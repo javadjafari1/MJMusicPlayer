aec0c6bb942da66534178de9ac744b24fc1a61fc
Mohammad Yazdi <mohammadyt1379@gmail.com>
2023-11-28 22:46:41 +0330

    #27 fix pullRequest issues by javad jafari - convert value class to data class for UpdateProgress in AudioEvent - write an interface for AudioHandler and implement it in AudioHandlerImpl class

--- app/src/main/kotlin/ir/thatsmejavad/mjmusic/di/AudioPlayerModule.kt
+++ app/src/main/kotlin/ir/thatsmejavad/mjmusic/di/AudioPlayerModule.kt
@@ -2,7 +2,9 @@ package ir.thatsmejavad.mjmusic.di
 
 import androidx.media3.exoplayer.ExoPlayer
 import ir.thatsmejavad.mjmusic.core.audioPlayer.AudioHandler
+import ir.thatsmejavad.mjmusic.core.audioPlayer.AudioHandlerImpl
 import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val audioPlayerModule = module {
@@ -12,7 +14,7 @@ val audioPlayerModule = module {
             .build()
     }
 
-    single {
-        AudioHandler(get())
+    single<AudioHandler> {
+        AudioHandlerImpl(get())
     }
 }
