0d934924de224cb1522cac42163184f189b883df
Mohammad Yazdi <mohammadyt1379@gmail.com>
2023-11-28 22:46:41 +0330

    #27 fix pullRequest issues by javad jafari - convert value class to data class for UpdateProgress in AudioEvent - write an interface for AudioHandler and implement it in AudioHandlerImpl class

--- app/src/main/kotlin/ir/thatsmejavad/mjmusic/core/audioPlayer/AudioHandler.kt
+++ app/src/main/kotlin/ir/thatsmejavad/mjmusic/core/audioPlayer/AudioHandler.kt
@@ -2,57 +2,25 @@ package ir.thatsmejavad.mjmusic.core.audioPlayer
 
 import androidx.annotation.FloatRange
 import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.common.Player
 
-class AudioHandler(private val player: ExoPlayer) {
-    init {
-        player.prepare()
-    }
-
-    fun addMediaItem(mediaItem: MediaItem) = player.addMediaItem(mediaItem)
-    fun addMediaItems(mediaItems: List<MediaItem>) = player.addMediaItems(mediaItems)
+interface AudioHandler : Player.Listener {
+    fun addMediaItem(mediaItem: MediaItem)
+    fun addMediaItems(mediaItems: List<MediaItem>)
 
 
-    fun removeMediaItem(index: Int) = player.removeMediaItem(index)
-    fun removeMediaItems(fromIndex: Int, toIndex: Int) = player.removeMediaItems(fromIndex, toIndex)
+    fun removeMediaItem(index: Int)
+    fun removeMediaItems(fromIndex: Int, toIndex: Int)
 
 
-    fun setMediaItem(mediaItem: MediaItem) = player.setMediaItem(mediaItem)
-
-    fun setMediaItems(mediaItems: List<MediaItem>) = player.setMediaItems(mediaItems)
-
-
-    fun setPlaybackSpeed(@FloatRange(from = 0.0, to = 2.0, fromInclusive = false) speed: Float) =
-        player.setPlaybackSpeed(speed)
-
-    fun setVolume(@FloatRange(from = 0.0, to = 1.0) volume: Float) {
-        player.volume = volume
-    }
-
-    fun getVolume() = player.volume
+    fun setMediaItem(mediaItem: MediaItem)
+    fun setMediaItems(mediaItems: List<MediaItem>)
 
 
-    fun setEvent(audioEvent: AudioEvent) {
-        when (audioEvent) {
-            AudioEvent.PlayPause -> handlePlayPauseEvent()
-            AudioEvent.Stop -> player.stop()
-            AudioEvent.Backward -> player.seekBack()
-            AudioEvent.Forward -> player.seekForward()
-            AudioEvent.SeekToNext -> player.seekToNextMediaItem()
-            AudioEvent.SeekToPrevious -> player.seekToPreviousMediaItem()
-            is AudioEvent.UpdateProgress -> player.seekTo(audioEvent.progressMs)
-            is AudioEvent.SeekTo -> player.seekTo(
-                audioEvent.mediaItemIndex,
-                audioEvent.positionMs
-            )
-        }
-    }
+    fun setPlaybackSpeed(@FloatRange(from = 0.0, to = 2.0, fromInclusive = false) speed: Float)
+    fun setVolume(@FloatRange(from = 0.0, to = 1.0) volume: Float)
 
-    private fun handlePlayPauseEvent() {
-        if (player.isPlaying) {
-            player.pause()
-        } else {
-            player.play()
-        }
-    }
+    fun getVolume(): Float
+
+    fun setEvent(audioEvent: AudioEvent)
 }
