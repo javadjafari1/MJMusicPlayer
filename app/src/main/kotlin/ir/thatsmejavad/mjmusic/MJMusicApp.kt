87aa077f994554cf64876c1ef9827918e6546c8d
Mohammad Yazdi <mohammadyt1379@gmail.com>
2024-01-12 16:29:52 +0330

    #31 - Rename AudioPlayerModule.kt to AudioModule.kt - Provide AudioProvider as a single instance in in AudioModule - add AudioModule to the app's modules

--- app/src/main/kotlin/ir/thatsmejavad/mjmusic/MJMusicApp.kt
+++ app/src/main/kotlin/ir/thatsmejavad/mjmusic/MJMusicApp.kt
@@ -2,7 +2,7 @@ package ir.thatsmejavad.mjmusic
 
 import android.app.Application
 import ir.thatsmejavad.mjmusic.di.databaseModule
-import ir.thatsmejavad.mjmusic.di.audioPlayerModule
+import ir.thatsmejavad.mjmusic.di.audioModule
 import ir.thatsmejavad.mjmusic.di.viewModelsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -17,7 +17,7 @@ class MJMusicApp : Application() {
             modules(
                 viewModelsModule,
                 databaseModule,
-                audioPlayerModule
+                audioModule
             )
         }
     }
