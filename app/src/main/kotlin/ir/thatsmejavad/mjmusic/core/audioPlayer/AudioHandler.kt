2436c538347ddc93db10cbdea3651a6d0b465c9c
Mohammad Yazdi <mohammadyt1379@gmail.com>
2024-01-12 16:44:55 +0330

    #31 Finalize AudioHandler interface and implement in AudioHandlerImpl

--- app/src/main/kotlin/ir/thatsmejavad/mjmusic/core/audioPlayer/AudioHandler.kt
+++ app/src/main/kotlin/ir/thatsmejavad/mjmusic/core/audioPlayer/AudioHandler.kt
@@ -1,26 +1,43 @@
 package ir.thatsmejavad.mjmusic.core.audioPlayer
 
 import androidx.annotation.FloatRange
-import androidx.media3.common.MediaItem
-import androidx.media3.common.Player
+import ir.thatsmejavad.mjmusic.data.db.entites.SongEntity
+import ir.thatsmejavad.mjmusic.enums.RepeatMode
+import kotlinx.coroutines.flow.StateFlow
 
-interface AudioHandler : Player.Listener {
-    fun addMediaItem(mediaItem: MediaItem)
-    fun addMediaItems(mediaItems: List<MediaItem>)
+interface AudioHandler {
 
+    val audioState: StateFlow<AudioState>
+    val nowPlayingSongs: StateFlow<List<SongEntity>>
 
-    fun removeMediaItem(index: Int)
-    fun removeMediaItems(fromIndex: Int, toIndex: Int)
+    suspend fun setSong(song: SongEntity)
+    suspend fun setSongs(songs: List<SongEntity>)
 
+    suspend fun addSongToEnd(song: SongEntity)
+    suspend fun addSongsToEnd(songs: List<SongEntity>)
 
-    fun setMediaItem(mediaItem: MediaItem)
-    fun setMediaItems(mediaItems: List<MediaItem>)
+    suspend fun addSongToNext(song: SongEntity)
+    suspend fun addSongsToNext(songs: List<SongEntity>)
 
+    suspend fun removeSong(index: Int)
+    suspend fun removeSongs(fromIndex: Int, toIndex: Int)
+    suspend fun removeAllSongs()
 
-    fun setPlaybackSpeed(@FloatRange(from = 0.0, to = 2.0, fromInclusive = false) speed: Float)
+    fun release()
+
+    fun setPlaybackSpeed(
+        @FloatRange(
+            from = 0.25,
+            to = 2.0,
+            fromInclusive = false
+        ) speed: Float
+    )
+
+    fun setRepeatMode(repeatMode: RepeatMode)
+
+    fun setIsShuffleModeEnabled(shuffleModeEnabled: Boolean)
+
     fun setVolume(@FloatRange(from = 0.0, to = 1.0) volume: Float)
 
-    fun getVolume(): Float
-
-    fun setEvent(audioEvent: AudioEvent)
+    fun sendEvent(audioEvent: AudioEvent)
 }
--- app/src/main/kotlin/ir/thatsmejavad/mjmusic/core/audioPlayer/AudioHandlerImpl.kt
+++ app/src/main/kotlin/ir/thatsmejavad/mjmusic/core/audioPlayer/AudioHandlerImpl.kt
@@ -2,55 +2,205 @@ package ir.thatsmejavad.mjmusic.core.audioPlayer
 
 import androidx.annotation.FloatRange
 import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackParameters
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
+import ir.thatsmejavad.mjmusic.data.db.entites.SongEntity
+import ir.thatsmejavad.mjmusic.enums.RepeatMode
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class AudioHandlerImpl(private val player: ExoPlayer) : AudioHandler {
     init {
         player.prepare()
+        setupListeners()
     }
 
-    override fun addMediaItem(mediaItem: MediaItem) = player.addMediaItem(mediaItem)
-    override fun addMediaItems(mediaItems: List<MediaItem>) = player.addMediaItems(mediaItems)
+    companion object {
+        private const val PROGRESS_UPDATE_INTERVAL = 1000L
+        private const val BACKWARD_THRESHOLD = 3000L
+    }
+
+    private val _audioState = MutableStateFlow(AudioState())
+    override val audioState = _audioState.asStateFlow()
+
+    private val _nowPlayingSongs = MutableStateFlow<List<SongEntity>>(emptyList())
+    override val nowPlayingSongs = _nowPlayingSongs.asStateFlow()
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var progressJob: Job? = null
 
 
-    override fun removeMediaItem(index: Int) = player.removeMediaItem(index)
-    override fun removeMediaItems(fromIndex: Int, toIndex: Int) =
+    private fun setupListeners() {
+        player.addListener(object : Player.Listener {
+
+            override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+                super.onMediaItemTransition(mediaItem, reason)
+                mediaItem?.let {
+                    _audioState.update {
+                        it.copy(
+                            currentSongIndex = player.currentMediaItemIndex,
+                            currentSongPosition = player.currentPosition
+                        )
+                    }
+                }
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                _audioState.update { it.copy(isPlaying = isPlaying) }
+                if (isPlaying) {
+                    startProgressUpdate()
+                } else {
+                    stopProgressUpdater()
+                }
+            }
+
+            override fun onVolumeChanged(volume: Float) {
+                super.onVolumeChanged(volume)
+                _audioState.update { it.copy(volume = volume) }
+            }
+
+            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
+                super.onPlaybackParametersChanged(playbackParameters)
+                _audioState.update { it.copy(playBackSpeed = playbackParameters.speed) }
+            }
+
+            override fun onIsLoadingChanged(isLoading: Boolean) {
+                super.onIsLoadingChanged(isLoading)
+                _audioState.update { it.copy(isLoading = isLoading) }
+            }
+
+            override fun onRepeatModeChanged(repeatMode: Int) {
+                super.onRepeatModeChanged(repeatMode)
+                _audioState.update { it.copy(repeatMode = RepeatMode.getFromValue(repeatMode)) }
+            }
+
+            override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
+                super.onShuffleModeEnabledChanged(shuffleModeEnabled)
+                _audioState.update { it.copy(isShuffleModeEnabled = shuffleModeEnabled) }
+            }
+        }
+        )
+    }
+
+    private fun startProgressUpdate() {
+        progressJob = coroutineScope.launch {
+            while (_audioState.value.isPlaying) {
+                _audioState.update { it.copy(currentSongPosition = player.currentPosition) }
+                delay(PROGRESS_UPDATE_INTERVAL)
+            }
+        }
+    }
+
+    private fun stopProgressUpdater() {
+        progressJob?.cancel()
+    }
+
+    override suspend fun setSong(song: SongEntity) {
+        _nowPlayingSongs.emit(listOf(song))
+        player.setMediaItem(song.toMediaItem())
+    }
+
+    override suspend fun setSongs(songs: List<SongEntity>) {
+        _nowPlayingSongs.emit(songs)
+        player.setMediaItems(songs.map { it.toMediaItem() })
+    }
+
+    override suspend fun addSongToEnd(song: SongEntity) {
+        _nowPlayingSongs.emit(_nowPlayingSongs.value + song)
+        player.addMediaItem(song.toMediaItem())
+    }
+
+    override suspend fun addSongsToEnd(songs: List<SongEntity>) {
+        _nowPlayingSongs.emit(_nowPlayingSongs.value + songs)
+        player.addMediaItems(songs.map { it.toMediaItem() })
+    }
+
+    override suspend fun addSongToNext(song: SongEntity) {
+        if (_nowPlayingSongs.value.isEmpty()) {
+            setSong(song)
+        } else {
+            val currentSongIndex = _audioState.value.currentSongIndex
+            _nowPlayingSongs.emit(
+                _nowPlayingSongs.value.toMutableList().apply { add(currentSongIndex + 1, song) }
+            )
+            player.addMediaItem(currentSongIndex + 1, song.toMediaItem())
+        }
+    }
+
+    override suspend fun addSongsToNext(songs: List<SongEntity>) {
+        if (_nowPlayingSongs.value.isEmpty()) {
+            setSongs(songs)
+        } else {
+            val currentSongIndex = _audioState.value.currentSongIndex
+            _nowPlayingSongs.emit(
+                _nowPlayingSongs.value.toMutableList().apply { addAll(currentSongIndex + 1, songs) }
+            )
+            player.addMediaItems(currentSongIndex + 1, songs.map { it.toMediaItem() })
+        }
+    }
+
+    override suspend fun removeSong(index: Int) {
+        _nowPlayingSongs.emit(_nowPlayingSongs.value.toMutableList().apply { removeAt(index) })
+        player.removeMediaItem(index)
+    }
+
+    override suspend fun removeSongs(fromIndex: Int, toIndex: Int) {
+        _nowPlayingSongs.emit(
+            _nowPlayingSongs.value.toMutableList().apply { subList(fromIndex, toIndex).clear() }
+        )
         player.removeMediaItems(fromIndex, toIndex)
+    }
 
+    override suspend fun removeAllSongs() {
+        _nowPlayingSongs.emit(emptyList())
+        player.clearMediaItems()
+    }
 
-    override fun setMediaItem(mediaItem: MediaItem) = player.setMediaItem(mediaItem)
-
-    override fun setMediaItems(mediaItems: List<MediaItem>) = player.setMediaItems(mediaItems)
-
+    override fun release() {
+        player.release()
+        coroutineScope.cancel()
+    }
 
     override fun setPlaybackSpeed(
         @FloatRange(
-            from = 0.0,
-            to = 2.0,
-            fromInclusive = false
+            from = 0.25,
+            to = 2.0
         ) speed: Float
-    ) =
-        player.setPlaybackSpeed(speed)
+    ) = player.setPlaybackSpeed(speed)
+
+    override fun setRepeatMode(repeatMode: RepeatMode) {
+        player.repeatMode = repeatMode.value
+    }
+
+    override fun setIsShuffleModeEnabled(shuffleModeEnabled: Boolean) {
+        player.shuffleModeEnabled = shuffleModeEnabled
+    }
 
     override fun setVolume(@FloatRange(from = 0.0, to = 1.0) volume: Float) {
         player.volume = volume
     }
 
-    override fun getVolume() = player.volume
-
-
-    override fun setEvent(audioEvent: AudioEvent) {
+    override fun sendEvent(audioEvent: AudioEvent) {
         when (audioEvent) {
             AudioEvent.PlayPause -> handlePlayPauseEvent()
-            AudioEvent.Stop -> player.stop()
-            AudioEvent.Backward -> player.seekBack()
-            AudioEvent.Forward -> player.seekForward()
-            AudioEvent.SeekToNext -> player.seekToNextMediaItem()
-            AudioEvent.SeekToPrevious -> player.seekToPreviousMediaItem()
-            is AudioEvent.UpdateProgress -> player.seekTo(audioEvent.progressMs)
-            is AudioEvent.SeekTo -> player.seekTo(
-                audioEvent.mediaItemIndex,
-                audioEvent.positionMs
+            AudioEvent.Stop -> handleStopEvent()
+            AudioEvent.Backward -> backward()
+            AudioEvent.Forward -> forward()
+            AudioEvent.SeekToNext -> seekToNext()
+            AudioEvent.SeekToPrevious -> seekToPrevious()
+            is AudioEvent.SeekToPosition -> seekToPosition(audioEvent.positionMs)
+            is AudioEvent.SeekToItem -> seekToItem(
+                index = audioEvent.index,
+                positionMs = audioEvent.positionMs
             )
         }
     }
@@ -61,5 +211,45 @@ class AudioHandlerImpl(private val player: ExoPlayer) : AudioHandler {
         } else {
             player.play()
         }
+    }
+
+    private fun handleStopEvent() {
+        player.pause()
+        player.seekTo(0)
+        _audioState.update { it.copy(currentSongPosition = 0) }
+    }
+
+    private fun forward() {
+        if (player.isPlaying) {
+            player.seekForward()
+        }
+    }
+
+    private fun backward() {
+        if (player.isPlaying) {
+            player.seekBack()
+        }
+    }
+
+    private fun seekToNext() {
+        player.seekToNextMediaItem()
+    }
+
+    private fun seekToPrevious() {
+        if (player.currentPosition > BACKWARD_THRESHOLD) {
+            player.seekTo(player.currentMediaItemIndex, 0)
+            _audioState.update { it.copy(currentSongPosition = 0) }
+        } else {
+            player.seekToPreviousMediaItem()
+        }
+    }
+
+    private fun seekToPosition(positionMs: Long) {
+        player.seekTo(positionMs)
+        _audioState.update { it.copy(currentSongPosition = positionMs) }
+    }
+
+    private fun seekToItem(index: Int, positionMs: Long) {
+        player.seekTo(index, positionMs)
     }
 }
